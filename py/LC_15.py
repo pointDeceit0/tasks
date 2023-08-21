8ce05f46427e05af9a6e2ce31c8ce6774b8f242e
pointDeceit0 <fedor.korolev.87@mail.ru>
2023-08-21 09:08:57 +0300

    New cycle

--- py/LC_15.py
+++ py/LC_15.py
@@ -1,66 +1,78 @@
+from collections import defaultdict
+
 class List(list):
     pass
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        if n < 3: return []
-        '''
-        time complexity --- O(n^3)
-             space      --- O(n)
+        # some fast shit
+        negative = defaultdict(int)
+        positive = defaultdict(int)
+        zeros = 0
+        for num in nums:
+            if num < 0:
+                negative[num] += 1
+            elif num > 0:
+                positive[num] += 1
+            else:
+                zeros += 1
+        
+        result = []
+        if zeros:
+            for n in negative:
+                if -n in positive:
+                    result.append((0, n, -n))       
+            if zeros > 2:
+                result.append((0,0,0))
 
-        ans = []
-        for f in range(n - 2):
-            for s in range(f + 1, n - 1):
-                for t in range(s + 1, n):
-                    if (nums[f] + nums[s] + nums[t]) == 0 and all(list(per) not in ans for per in permutations((nums[f], nums[s], nums[t]), r=3)):
-                        ans.append([nums[f], nums[s], nums[t]])'''
+        for set1, set2 in ((negative, positive), (positive, negative)):
+            set1Items = list(set1.items())
+            for i, (j, k) in enumerate(set1Items):
+                for j2, k2 in set1Items[i:]:
+                    if j != j2 or (j == j2 and k > 1):
+                        if -j-j2 in set2:
+                            result.append((j, j2, -j-j2))
+        return result
+    
+    class Solution:
+        def threeSum1(self, nums: List[int]) -> List[List[int]]:
+            # Mine one 
+            ans = []
+            nums.sort()
+            prev_v = None
 
-        '''
-        experiments if have to return indexes of numbers
+            for i in range(len(nums)):
+                if nums[i] == prev_v:
+                    continue
+                l, r = i + 1, len(nums) - 1
+                prev_l = None
 
-        nums = list(enumerate(nums))
-        nums.sort(key=lambda x: x[1])
-        nums = [(tup[1], tup[0]) for tup in nums]
-        nums = dict(nums)'''
-
-        nums.sort()
-        ans = []
-
-        '''
-        time  --- O(n^2)
-        space --- O(n)
-        '''
-
-        for i in range(n - 2):
-            if nums[i] == nums[i - 1] and i != 0:
-                # условие ломается, когда в массиве все числа одинаковые, кроме случая, когда все числа 0. 
-                continue
-            l, r = i + 1, n - 1
-
-            while l != r:
-                if nums[i] + nums[l] + nums[r] == 0:
-                    ans.append([nums[i], nums[l], nums[r]])
-
-                    cur_l = l
-                    while nums[cur_l] == nums[l] and l != r:
+                while l < r:
+                    if nums[l] == prev_l or nums[i] + nums[l] + nums[r] < 0:
+                        prev_l = nums[l]
                         l += 1
+                        continue
+                    elif nums[i] + nums[l] + nums[r] > 0:
+                        r -= 1
+                        continue
 
-                elif nums[i] + nums[l] + nums[r] < 0:
-                    l += 1
+                    elif nums[i] + nums[l] + nums[r] == 0:
+                        ans.append([nums[i], nums[l], nums[r]])
 
-                elif nums[i] + nums[l] + nums[r] > 0:
-                    r -= 1
+                        prev_l = nums[l]
+                prev_v = nums[i]
 
-        return ans
+            return ans
+
 
 
 def test():
     a = Solution()
 
+    assert a.threeSum([3,0,-2,-1,1,2]) == [[3, -2, -1], [0, -2, 2], [0, -1, 1]]
     assert a.threeSum([-1,0,1,2,-1,-4]) == [[-1,-1,2],[-1,0,1]]
+    assert a.threeSum([0, 0, 0, 0, 0, 0, 0, 0]) == [[0, 0, 0]]
     assert a.threeSum([]) == []
     assert a.threeSum([0]) == []
-    assert a.threeSum([0, 0, 0, 0, 0, 0, 0, 0]) == [0, 0, 0]
 
 test()
