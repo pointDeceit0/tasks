1871c8482fa0d80502e8fcb9f092f694910cdfc0
pointDeceit0 <fedor.korolev.87@mail.ru>
2023-08-23 10:39:42 +0300

    New cycle

--- py/LC_70.py
+++ py/LC_70.py
@@ -7,6 +7,12 @@ class Solution:
             first, second = second, first + second
 
         return second
+        '''
+        stairs = [1, 1] + [0] * (n - 1)
+        for i in range(2, n + 2):
+            stairs[i] = stairs[i - 1] + stairs[i - 2]
+        
+        return stairs[-1]'''
 
 
 def main():
