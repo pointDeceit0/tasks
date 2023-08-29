0856c078d9bd72a73293ca6916902358b453fe0f
pointDeceit0 <fedor.korolev.87@mail.ru>
2023-08-29 21:26:20 +0300

    New cycle

--- py/LC_91.py
+++ py/LC_91.py
@@ -1,37 +1,51 @@
-import string
-
-
 class Solution:
-    encode = {str(i): v for i, v in enumerate(string.ascii_uppercase, 1)}
-
     def numDecodings(self, s: str) -> int:
-        '''
-        time  --- O(n)
-        space --- O(n)
-        '''
-        cache = [-1] * (len(s) - 1)
-
-        def _dp(i: int):
-            if i >= len(s) - 1:
-                if i == len(s) - 1 and s[i] == "0":
+        ''' Optimal
+        class Solution:
+            def numDecodings(self, s: str) -> int:
+                if s[0] == '0':
                     return 0
+                n = len(s)
+                dp = [0 for _ in range(n + 1)]
+                dp[0] = 1
+                for i in range(1, n + 1):
+                    if s[i - 1] != '0':
+                        dp[i] = dp[i - 1]
+                    if i >= 2:
+                        x = int(s[i - 2: i])
+                        if 10 <= x <= 26:
+                            dp[i] += dp[i - 2]
+                return dp[n]
+        '''
+        
+        # mine
+        # time  --- O(?)
+        # space --- O(n)
+        cache = [0] * len(s)
+        def _dfs(i):
+            if i >= len(s):
                 return 1
-            # if in cache
-            if cache[i] != -1: return cache[i]
+            if s[i] == '0':
+                return 0
+                
+            if cache[i]:
+                return cache[i]
             
-            # _ case and _._ case
-            cache[i] = (_dp(i + 1) if s[i : i + 1] in self.encode else 0) + \
-                       (_dp(i + 2) if s[i : i + 2] in self.encode else 0)
-            
+            cache[i] = _dfs(i + 1) + (_dfs(i + 2) if 10 <= int(s[i:i + 2]) <= 26 else 0)
+                
             return cache[i]
 
-        return _dp(0)
-                
+        _dfs(0)
+        return cache[0]
+
+            
 
 def test():
     s = Solution()
 
+    assert s.numDecodings("1123") == 5
     assert s.numDecodings("12345") == 3
+    assert s.numDecodings("120214") == 3
     assert s.numDecodings("12") == 2
     assert s.numDecodings("226") == 3
     assert s.numDecodings("26") == 2
