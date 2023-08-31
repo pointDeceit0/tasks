cd7ede6098ffe55f9a1d3a0924cb60739ece6aa2
pointDeceit0 <fedor.korolev.87@mail.ru>
2023-08-31 13:00:13 +0300

    New cycle

--- py/LC_207.py
+++ py/LC_207.py
@@ -5,51 +5,66 @@ class List(list):
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         '''
-        it will cache right in the graph
+        Mine ~ 10 times slower than the one below
 
-        time  O(n)
-        space O(n), actually O(n) + O(n)
-        '''
-        graph = {}
-        for v, p in prerequisites:
-            if v not in graph:
-                graph[v] = {p}
-            else:
-                graph[v].add(p)
+        nodes = {}
+
+        def check(node: int, leaves: List[int], already_checked: List[int]):
             
-        def dfs(v: int, were: set) -> bool:
-            # check if in cache -> true
-            if graph.get(v) == True:
-                return True
-
-            # if vertex in graph(like key) -> going by each its linked vertexes
-            if graph.get(v, 0):
-                for prereq in graph[v]:
-                    if graph.get(prereq) == True:
-                        continue
-                    
-                    if prereq in were:
+            for v in leaves:
+                if v in already_checked:
+                    continue
+                if node == v:
+                    return False    
+                if v in nodes:
+                    already_checked.add(v)
+                    if not check(node, nodes[v], already_checked):
                         return False
-                    # condition to get rid of unnecessary recursion AND if in cache
-                    if graph.get(prereq, False):# and not cache.get(v, False):
-                        were.add(prereq)
-                        if not dfs(prereq, were):
+                    
+            return True
+
+            for leaf, node in prerequisites:
+                if node not in nodes:
+                    nodes[node] = {leaf}
+                else:
+                    nodes[node].add(leaf)
+                if not check(node, nodes[node], set()):
+                    return False
+            
+            return True
+        '''
+        # Most efficient
+        nodes = {}
+
+        def check(node: int, already_checked: List[int]):
+            if nodes.get(node) == True: return True
+            
+            if nodes.get(node):
+                for v in nodes[node]:
+                    if nodes.get(v) == True: continue
+                    if v in already_checked: return False
+                    if v in nodes:
+                        already_checked.add(v)
+                        if not check(v, already_checked):
                             return False
                         
-                graph[v] = True
-                return True
+                nodes[node] = True
+            return True
 
+        for leaf, node in prerequisites:
+            if node not in nodes:
+                nodes[node] = {leaf}
             else:
-                return True
+                nodes[node].add(leaf)
         
-        return all(dfs(i, set()) for i in graph.keys())
+        return all(check(n, set()) for n in nodes.keys())
 
 
 def test():
     a = Solution()
 
-    assert a.canFinish(5, [[1, 2], [3, 4], [3, 1], [5, 1], [4, 5], [4, 1], [5, 2]]) == True
     assert a.canFinish(2, [[1,0],[0,1]]) == False
+    assert a.canFinish(5, [[1, 2], [3, 4], [3, 1], [5, 1], [4, 5], [4, 1], [5, 2]]) == True
     assert a.canFinish(4, [[1, 2], [2, 3], [3, 4], [4, 1]]) == False
     assert a.canFinish(4, [[1, 2], [2, 3], [3, 1]]) == False
     assert a.canFinish(0, []) == True
